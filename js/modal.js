be1374066d50269c0ec77ee1788d3774ac49bd7d
OlgaBrekhuntsova <olga.pistryak@gmail.com>
2020-09-21 11:54:04 +0300

    Блокирует прокрутку body при открытии формы и бургера
    Нужно добавить scroll бургера и формы.

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,19 @@
 (() => {
-    const refs = {
-      openModalBtn: [...document.querySelectorAll("[data-modal-open]")],
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.forEach((e) => {
-      e.addEventListener("click", toggleModal);
-    });
-  
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: [...document.querySelectorAll('[data-modal-open]')],
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    bodyModal: document.querySelector('[body-modal]'),
+  };
+
+  refs.openModalBtn.forEach(e => {
+    e.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.bodyModal.classList.toggle('is-unlocked'),
+      refs.modal.classList.toggle('is-hidden');
+  }
+})();
